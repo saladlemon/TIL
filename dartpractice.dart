f5c9a73ab92a878e8b99d307ed715ee2c200ff98
saladlemon <wjsehgns11@naver.com>
2022-12-20 19:15:46 +0900

    dartpractice

--- dartpractice.dart
+++ dartpractice.dart
@@ -1,3 +1,39 @@
-void main(){
-  print('good job');
+void main() {
+  // string interpolation
+  var name = 'hoon';
+  var age = 28;
+  var greeting =
+      "Hello everyone, my name is $name, and I'm ${age} nice to meet you!";
+  print(greeting);
+  // collection for
+  var oldFriends = ['nico', 'lynn'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren',
+    for (var friend in oldFriends) "Heart $friend",
+  ];
+  print(newFriends);
+  //maps
+  var player = {
+    'name': 'nico',
+    'xp': 19.99,
+    'superpower': false,
+  };
+  //sets
+  Set<int> numbers = {1, 2, 3, 4};
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+  print(numbers);
+  //defining a funcion
+  sayHello(name);
+  plus(4, 5);
 }
+
+void sayHello(String name) {
+  print("Hello $name nice to meet you");
+}
+
+num plus(num a, num b) => a + b;
