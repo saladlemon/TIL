377bc608b2bb59bface001dd8af29839dd3dc380
saladlemon <wjsehgns11@naver.com>
2023-06-29 15:06:46 +0900

    hi

--- practiceJS.js
+++ practiceJS.js
@@ -1,27 +1,14 @@
-function solution(array) {
-  const length = array.length;
-  let mode;
-  let Subcount = 0;
-  let count = 1;
+// 배열.map(함수를 담습니다.) // 새로운 값을 가공할 함수 또는 로직
 
-  if (length === 1) {
-    mode = array[0];
-  } else {
-    for (i = 1; i < length; i++) {
-      if (array[i - 1] === array[i]) {
-        count = count + 1;
-      } else {
-        if (Subcount < count) {
-          Subcount = count;
-          mode = array[i];
-        } else if (Subcount === count) {
-          mode = -1;
-        } else {
-          count = 0;
-        }
-      }
-    }
-  }
-  let answer = mode;
-  return answer;
-}
+// 배열의 요소만큼 map에 작성된 함수가 수행됩니다. 인자는 두개를 받습니다.
+
+// 1. 현재 요소의 값, 2. 현재 배열의 크기
+
+// example
+const a = [1, 2, 3, 4, 5];
+
+const unit = a.map((unit, idx) => {
+  return unit - 1;
+});
+// unit: [0, 1, 2, 3, 4]
+console.log(unit);
